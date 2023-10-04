bb39c3cbb74cf0dc81919779ab4b1ca67010802f
GTM106 <raito125dragon7@gmail.com>
2023-10-04 18:03:04 +0900

    Shaderの基本機能を実装しました

--- src1_anim/Assets/Scripts/RotateBehaviourScript.cs
+++ src1_anim/Assets/Scripts/RotateBehaviourScript.cs
@@ -6,5 +6,8 @@ public class RotateBehaviourScript : MonoBehaviour
 {
     void Update()
     {
+        float rotate = 360f * Time.deltaTime / 10f;
+
+        transform.Rotate(0f, rotate, 0f);
     }
 }
